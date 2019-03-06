ecc0d3a5f6416f7427601ebafa03cea4504369b4
James Hunter <james.hunter@hootsuite.com>
2019-03-06 09:10:40 -0800

    gzip

--- lib/blog_web/endpoint.ex
+++ lib/blog_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule BlogWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :blog,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
