7841eb29ebe722ea1ea78e5df4bf19328f84dae5
James Hunter <james.hunter@hootsuite.com>
2019-03-07 08:54:58 -0800

    assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
+import "./segment"
 import css from "../css/app.css"
 
 import "phoenix_html"
 
 // import socket from "./socket"
 
-import hljs from "highlight.js"
 import "highlight.js/styles/dracula.css"
-
-hljs.initHighlightingOnLoad()
+import("highlight.js").then(hljs => {
+  hljs.initHighlightingOnLoad()
+})
